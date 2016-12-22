e0c5294d8676c8be8fd946113f4a3c6b6d218f22
Anton Gushcha <ncrashed@gmail.com>
2016-12-22 16:57:44 +0300

    Execution of server application

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,42 @@
 module Main where
 
+import Network.Wai.Handler.Warp
+import Network.Wai.Middleware.RequestLogger
+import Options.Applicative
+
+import Text.PDF.Slave.Server
+
+-- | Argument line options
+data Options = Options {
+  -- | Path to config, if not set, the app will not start
+  configPath :: FilePath
+}
+
+-- | Command line parser
+optionsParser :: Parser Options
+optionsParser = Options
+  <$> strOption (
+         long "conf"
+      <> metavar "CONFIG"
+      <> help "Path to configuration file"
+    )
+
+-- | Execute server with given options
+runServer :: Options -> IO ()
+runServer Options{..} = do
+  cfg <- readConfig configPath
+  env <- newServerEnv cfg
+  let logger = makeLogger $ serverDetailedLogging cfg
+  run (serverPort cfg) $ logger $ pdfSlaveServerApp env
+  where
+    makeLogger b = if b
+      then logStdoutDev
+      else logStdout
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = execParser opts >>= runServer
+  where
+    opts = info (helper <*> optionsParser)
+      ( fullDesc
+     <> progDesc "Web server for pdf-slave tool"
+     <> header "pdf-slave-server - provides web service for pdf-slave tool" )
--- src/Text/PDF/Slave/Server.hs
+++ src/Text/PDF/Slave/Server.hs
@@ -1,3 +1,29 @@
 module Text.PDF.Slave.Server(
-
+  -- * Server config
+    ServerConfig(..)
+  , readConfig
+  -- * Server environment
+  , ServerEnv
+  , newServerEnv
+  -- * Execution of server
+  , pdfSlaveServerApp
   ) where
+
+import Servant.Server
+import Data.Proxy
+
+import Text.PDF.Slave.Server.API
+import Text.PDF.Slave.Server.Config
+import Text.PDF.Slave.Server.Monad
+
+-- | Full Server API
+type ServerAPI = PDFSlaveAPI
+
+-- | WAI application of server
+pdfSlaveServerApp :: ServerEnv -> Application
+pdfSlaveServerApp e = serve (Proxy :: Proxy ServerAPI) $ enter (serverMtoHandler e) $
+  pdfSlaveServer
+
+-- | Implementation of main server API
+pdfSlaveServer :: ServerT PDFSlaveAPI ServerM
+pdfSlaveServer = undefined
